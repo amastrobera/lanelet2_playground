42e0f98ee1a7b7b33e259300532ab708d7b85c46
Angelo Mastroberardino <angelo@parkopedia.com>
2019-06-07 20:01:02 +0100

    gdal shapes for notebook and new test

--- docker/install_jupyter.sh
+++ docker/install_jupyter.sh
@@ -4,5 +4,6 @@ source /home/developer/pyenv/bin/activate
 pip2 install ipykernel jupyter # notebook
 pip2 install RISE   # presentation in jupyter notebook
 pip2 install rosdep # required to build lanelet2
+pip2 install pygdal==1.11.3.3 shapely # geometry operations
 python2 -m ipykernel install --user --name python2 --display-name "Python 2"
 deactivate
